f0f87fa1f70f9d4a658c0d318d3aa36c50cc5e09
Tomás <jtomasdias00@gmail.com>
2024-07-24 14:37:54 +0100

    change: updated Metadata

--- app/layout.tsx
+++ app/layout.tsx
@@ -4,7 +4,7 @@ import { Inter } from "next/font/google";
 const inter = Inter({ subsets: ["latin"] });
 
 export const metadata = {
-  title: "Self Hosted Tina App",
+  title: "Atlantinivel",
   description: "A Next.js app with TinaCMS",
 };
 
